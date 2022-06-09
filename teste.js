8285475873b068a1d528e279f69d112c397f326b
Bagre <100727470+Bagr3@users.noreply.github.com>
2022-06-09 13:21:49 -0400

    Add files via upload

--- teste.js
+++
@@ -1,9 +0,0 @@
-const cursorRounded = document.querySelector('.rounded');
-const cursorPointed = document.querySelector('.pointed');
-const moveCursor = (e)=> {  
-    const mouseY = e.clientY;  
-    const mouseX = e.clientX;     
-    cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;    
-    cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`; 
-}
-window.addEventListener('mousemove', moveCursor);
